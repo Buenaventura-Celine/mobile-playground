4c761afc528856a163fe53500d74512551c81c08
Celine Joie Buenaventura <celine@wizy.io>
2025-10-22 19:36:48 +0800

    update documentation and prevent android from handling the nfc intent

--- android/app/src/main/kotlin/com/example/mobile_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mobile_playground/MainActivity.kt
@@ -1,5 +1,42 @@
 package com.example.mobile_playground
-
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private var nfcAdapter: NfcAdapter? = null
+    private var pendingIntent: PendingIntent? = null
+
+    override fun onResume() {
+        super.onResume()
+        
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        
+        if (nfcAdapter != null) {
+            val intent = Intent(this, javaClass).apply {
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            }
+            
+            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_MUTABLE
+            } else {
+                0
+            }
+            
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, flags)
+            nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
+}
